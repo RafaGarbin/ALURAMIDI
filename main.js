9d09d50b325186340dd020a25a9f4ff392435f4c
RafaGarbin <107377830+RafaGarbin@users.noreply.github.com>
2023-11-01 14:22:05 -0300

    tt

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-function tocaSomAlausos(){
-    document.querySelector('#som_tecla_aplausos').play();
-}
+function tocaSomAplausos(  ){
+    document.querySelector ('#som_tecla_aplausos').play(  );             }
 document.querySelector('.tecla_aplausos').onclick=tocaSomAplausos;
-
-function tocaSomVaia(  ){
-    document.querySelector ('#som_tecla_vaia').play(  );             }
-document.querySelector('.tecla_vaia').onclick=tocaSomVaia;
