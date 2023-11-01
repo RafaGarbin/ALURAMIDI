a666bff3819a8cd57ae533bc0b8740f093484299
RafaGarbin <107377830+RafaGarbin@users.noreply.github.com>
2023-11-01 10:17:44 -0300

    oi

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ function tocaSomAplausos(){
     document.querySelector('#som_tecla_aplausos').play();
 }
 document.querySelector('.tecla_aplausos').onclick=tocaSomAplausos;
+
+function tocaSomAplausos(){
+    document.querySelector('#som_tecla_aplausos').play();
+}
+document.querySelector('.tecla_aplausos').onclick=tocaSomAplausos;
+F
+unction tocaSomAplausos(){
+    document.querySelector('#som_tecla_aplausos').play();
+}
+document.querySelector('.tecla_aplausos').onclick=tocaSomAplausos;
