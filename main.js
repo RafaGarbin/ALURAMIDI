8b9f1cf9b24ee5560e0cb55f10e7c7a016f8ab19
RafaGarbin <107377830+RafaGarbin@users.noreply.github.com>
2023-11-01 14:19:02 -0300

    ok

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function tocaSomAlausos(){
-    document.querySelector('#som_tecla_aplausos').play();
-}
+function tocaSomAplausos(  ){
+    document.querySelector ('#som_tecla_aplausos').play(  );             }
 document.querySelector('.tecla_aplausos').onclick=tocaSomAplausos;
+
