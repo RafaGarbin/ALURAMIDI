752fcdcb71b2ace458b2b7041418aa3454e72885
RafaGarbin <107377830+RafaGarbin@users.noreply.github.com>
2023-11-01 14:23:58 -0300

    vaia

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 function tocaSomAplausos(  ){
     document.querySelector ('#som_tecla_aplausos').play(  );             }
 document.querySelector('.tecla_aplausos').onclick=tocaSomAplausos;
+
+function tocaSomVaia(  ){
+    document.querySelector ('#som_tecla_vaia').play(  );             }
+document.querySelector('.tecla_vaia').onclick=tocaSomVaia;
