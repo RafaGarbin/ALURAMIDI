dfe2c005d977e27b3f98dfad82687061dd8ce566
ororo7 <128612640+ororo7@users.noreply.github.com>
2023-11-01 12:21:55 -0300

    ok

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 function tocaSomAplauso(  ){
     document.querySelector('# som_tecla_aplauso').play( );
 }
-
 document.querySelector('.tecla_aplauso').onclick=tocaSomAplauso;
+
+function tocsSomError(){
+    document.querySelector
+}
