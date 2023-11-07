819473b0e9aaf5e16260c04035811fb9d46cefd7
RafaGarbin <107377830+RafaGarbin@users.noreply.github.com>
2023-11-07 20:57:11 -0300

    ok

--- main.js
+++ main.js
@@ -5,3 +5,38 @@ document.querySelector('.tecla_aplausos').onclick=tocaSomAplausos;
 function tocaSomVaia(  ){
     document.querySelector ('#som_tecla_vaia').play(  );             }
 document.querySelector('.tecla_vaia').onclick=tocaSomVaia;
+
+function tocaSomRisadas(  ){
+    document.querySelector ('#som_tecla_risadas').play(  );             }
+document.querySelector('.tecla_risadas').onclick=tocaSomRisadas;
+
+
+
+
+function tocaSomErrado(  ){
+    document.querySelector ('#som_tecla_errado').play(  );             }
+document.querySelector('.tecla_errado').onclick=tocaSomErrado;
+
+function tocaSomGrilo(  ){
+    document.querySelector ('#som_tecla_grilo').play(  );             }
+document.querySelector('.tecla_grilo').onclick=tocaSomGrilo;
+
+function tocaSomTambores(  ){
+    document.querySelector ('#som_tecla_tambores').play(  );             }
+document.querySelector('.tecla_tambores').onclick=tocaSomTambores;
+
+
+
+
+function tocaSomTrombeta(  ){
+    document.querySelector ('#som_tecla_trombeta').play(  );             }
+document.querySelector('.tecla_trombeta').onclick=tocaSomTrombeta;
+
+function tocaSomDinheiro(  ){
+    document.querySelector ('#som_tecla_dinheiro').play(  );             }
+document.querySelector('.tecla_dinheiro').onclick=tocaSomDinheiro;
+
+
+function tocaSomVitoria(  ){
+    document.querySelector ('#som_tecla_vitoria').play(  );             }
+document.querySelector('.tecla_vitoria').onclick=tocaSomVitoria;
